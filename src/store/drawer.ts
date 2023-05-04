9a94d95e91e8b20cb33aac2fbd6616603815846d
tyange <usun16@gmail.com>
2023-05-04 21:39:52 +0900

    remove drawer store

--- src/store/drawer.ts
+++
@@ -1,9 +0,0 @@
-import { create } from "zustand";
-
-export const useDrawerStore = create<{
-  isDrawerOpen: boolean;
-  drawerToggle: () => void;
-}>((set) => ({
-  isDrawerOpen: false,
-  drawerToggle: () => set((state) => ({ isDrawerOpen: !state.isDrawerOpen })),
-}));
