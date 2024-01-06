bf1b2b36a484df2d7b8acb964e544f0f505ffeb7
tyange <usun16@gmail.com>
2024-01-06 23:12:41 +0900

    add text animation

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,9 +1,25 @@
 import { TypeAnimation } from "react-type-animation";
+import { motion } from "framer-motion";
 
 export default function Intro() {
+  const textVariant = {
+    initial: {
+      x: -500,
+      opacity: 0,
+    },
+    animate: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        duration: 2,
+        staggerChildren: 0.1,
+      },
+    },
+  };
+
   return (
     <div className="relative w-full h-full">
-      <div className="absolute top-1/4 left-5">
+      <div className="absolute flex flex-col gap-10 top-1/4 left-5 w-[764px]">
         <TypeAnimation
           sequence={[
             "한국문학의",
@@ -16,6 +32,28 @@ export default function Intro() {
           cursor={true}
           className="inline-block font-serif break-words text-7xl"
         />
+        <motion.div
+          className="mr-10"
+          variants={textVariant}
+          initial="initial"
+          animate="animate"
+        >
+          <motion.p className="text-right" variants={textVariant}>
+            한국문학은 세대를 거치며 변화를 거듭해왔습니다.
+          </motion.p>
+          <motion.p className="text-right" variants={textVariant}>
+            시대를 투영해 씌여진 새로운 한국문학 작품은
+          </motion.p>
+          <motion.p className="text-right" variants={textVariant}>
+            다시 한번 독자들을 놀라게 합니다.
+          </motion.p>
+          <motion.p className="text-right" variants={textVariant}>
+            또한 번역된 한국문학은 세계의 유수한 문학상을 거머쥐며
+          </motion.p>
+          <motion.p className="text-right" variants={textVariant}>
+            동아시아 문학의 새로운 길을 제시하고 있습니다.
+          </motion.p>
+        </motion.div>
       </div>
     </div>
   );
