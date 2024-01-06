48198de13b0d0415ffabb9a701c0266c47fba0d6
tyange <usun16@gmail.com>
2024-01-06 13:15:21 +0900

    add main page

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,10 @@
+import MainLayout from "../components/layouts/MainLayout.tsx";
+
+export default function MainPage() {
+  return (
+    <MainLayout>
+      <section>section 1</section>
+      <section>section 2</section>
+    </MainLayout>
+  );
+}
